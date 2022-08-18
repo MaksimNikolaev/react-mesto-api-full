5bbad8c3a698313b4d5a7630cd83accb300e7a48
Maks Nikolaev(work) <mksniklaev@gmail.com>
2022-08-18 12:31:52 +0500

    fix: use cors

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,6 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'http://mesto.nikolaev.nomoredomains.sbs/',
-    'https://mesto.nikolaev.nomoredomains.sbs/',
-    'http://api.mesto.nikolaev.nomoredomains.sbs/',
-    'https://api.mesto.nikolaev.nomoredomains.sbs/',
-  ],
-  credentials: true,
-};
-
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -27,7 +16,16 @@ app.use(express.json());
 
 app.use(requestLogger); // подключаем логгер запросов
 
-app.use('*', cors(options));
+app.use(cors({
+  credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'http://mesto.nikolaev.nomoredomains.sbs/',
+    'https://mesto.nikolaev.nomoredomains.sbs/',
+    'http://api.mesto.nikolaev.nomoredomains.sbs/',
+    'https://api.mesto.nikolaev.nomoredomains.sbs/',
+  ],
+}));
 
 app.use(routes); // подключаем роуты
 
